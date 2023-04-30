f8bd37e254ce178043531e18d6573f87fcc1ad10
Eduardo Hernandez <edhernandez@birt.eus>
2023-04-30 22:55:30 +0200

    Commit inicial con proyecto Spring Boot

---
+++ src/main/java/eus/birt/dam/app/Ud7TareaEvaluativa1Ejercicio2Application.java
@@ -0,0 +1,13 @@
+package eus.birt.dam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud7TareaEvaluativa1Ejercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud7TareaEvaluativa1Ejercicio2Application.class, args);
+	}
+
+}
---
+++ src/test/java/eus/birt/dam/app/Ud7TareaEvaluativa1Ejercicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eus.birt.dam.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud7TareaEvaluativa1Ejercicio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
