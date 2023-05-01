ab9288384c4bbeb2c634f7ed7175bc255151928a
Eduardo Hernandez <edhernandez@birt.eus>
2023-05-01 12:51:54 +0200

    Crear _id y simplificar clases eus.birt.dam.domain con Lombok

--- src/main/java/eus/birt/dam/domain/Geometry.java
+++ src/main/java/eus/birt/dam/domain/Geometry.java
@@ -1,20 +1,21 @@
 
 package eus.birt.dam.domain;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import jakarta.annotation.Generated;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "type", "coordinates" })
+
+@Data
+@NoArgsConstructor
 @Generated("jsonschema2pojo")
 public class Geometry {
 
@@ -22,37 +23,5 @@ public class Geometry {
 	private String type;
 	@JsonProperty("coordinates")
 	private List<Double> coordinates;
-	@JsonIgnore
-	private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
-
-	@JsonProperty("type")
-	public String getType() {
-		return type;
-	}
-
-	@JsonProperty("type")
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@JsonProperty("coordinates")
-	public List<Double> getCoordinates() {
-		return coordinates;
-	}
-
-	@JsonProperty("coordinates")
-	public void setCoordinates(List<Double> coordinates) {
-		this.coordinates = coordinates;
-	}
-
-	@JsonAnyGetter
-	public Map<String, Object> getAdditionalProperties() {
-		return this.additionalProperties;
-	}
-
-	@JsonAnySetter
-	public void setAdditionalProperty(String name, Object value) {
-		this.additionalProperties.put(name, value);
-	}
 
 }
--- src/main/java/eus/birt/dam/domain/Kulturklik.java
+++ src/main/java/eus/birt/dam/domain/Kulturklik.java
@@ -1,81 +1,34 @@
 
 package eus.birt.dam.domain;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import jakarta.annotation.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "id", "type", "geometry", "properties" })
+@JsonPropertyOrder({ "_id","id", "type", "geometry", "properties" })
+@Data
+@AllArgsConstructor
 @Generated("jsonschema2pojo")
-public class Kulturklik {
+@Document(collection="kulturklikData")
 
+public class Kulturklik {
+	
+	@JsonProperty("_id")
+	private String id;
 	@JsonProperty("id")
-	private Integer id;
+	private Integer eventoId;
 	@JsonProperty("type")
 	private String type;
 	@JsonProperty("geometry")
 	private Geometry geometry;
 	@JsonProperty("properties")
 	private Properties properties;
-	@JsonIgnore
-	private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
-
-	@JsonProperty("id")
-	public Integer getId() {
-		return id;
-	}
-
-	@JsonProperty("id")
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	@JsonProperty("type")
-	public String getType() {
-		return type;
-	}
-
-	@JsonProperty("type")
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@JsonProperty("geometry")
-	public Geometry getGeometry() {
-		return geometry;
-	}
-
-	@JsonProperty("geometry")
-	public void setGeometry(Geometry geometry) {
-		this.geometry = geometry;
-	}
-
-	@JsonProperty("properties")
-	public Properties getProperties() {
-		return properties;
-	}
-
-	@JsonProperty("properties")
-	public void setProperties(Properties properties) {
-		this.properties = properties;
-	}
-
-	@JsonAnyGetter
-	public Map<String, Object> getAdditionalProperties() {
-		return this.additionalProperties;
-	}
-
-	@JsonAnySetter
-	public void setAdditionalProperty(String name, Object value) {
-		this.additionalProperties.put(name, value);
-	}
 
 }
